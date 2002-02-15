5ffc734ac7e7254f3e77e0816014b875c8c4748b
Eric Sandeen <sandeen@sgi.com>
2002-02-15 23:06:21 +0000

    Sync up userspace headers for osyncisosync change

--- include/xfs_mount.h
+++ include/xfs_mount.h
@@ -343,7 +343,8 @@ typedef struct xfs_mount {
 #define XFS_MOUNT_NORECOVERY   	0x00000400      /* no recovery - dirty fs */
 #define XFS_MOUNT_SHARED    	0x00000800      /* shared mount */
 #define XFS_MOUNT_DFLT_IOSIZE  	0x00001000      /* set default i/o size */
-#define XFS_MOUNT_OSYNCISDSYNC 	0x00002000      /* treat o_sync like o_dsync */
+#define XFS_MOUNT_OSYNCISOSYNC 	0x00002000      /* o_sync is REALLY o_sync */
+						/* osyncisdsync is now default*/
 #define XFS_MOUNT_NOUUID	0x00004000	/* ignore uuid during mount */
 #define XFS_MOUNT_32BITINODES	0x00008000	/* do not create inodes above
 						 * 32 bits in size */
