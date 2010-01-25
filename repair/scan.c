bf0da4503cc554876f9840d3cbb8c297ca9640e3
Eric Sandeen <sandeen@sandeen.net>
2010-01-25 09:34:18 -0600

    repair: add missing newlines to scan.c
    Commit c83b756da0dac727457022d88d2a0ace30a6dc09 added some new
    do_warn's but omitted the "\n" leading to some pretty messy
    output when we go down this path.
    
    Signed-off-by: Eric Sandeen <sandeen@sandeen.net>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- repair/scan.c
+++ repair/scan.c
@@ -563,13 +563,13 @@ _("%s freespace btree block claimed (state %d), agno %d, bno %d, suspect %d\n"),
 
 			if (b == 0 || !verify_agbno(mp, agno, b)) {
 				do_warn(
-	_("invalid start block %u in record %u of %d btree block %u/%u"),
+	_("invalid start block %u in record %u of %d btree block %u/%u\n"),
 					b, i, name, agno, bno);
 				continue;
 			}
 			if (len == 0 || !verify_agbno(mp, agno, end - 1)) {
 				do_warn(
-	_("invalid length %u in record %u of %d btree block %u/%u"),
+	_("invalid length %u in record %u of %d btree block %u/%u\n"),
 					len, i, name, agno, bno);
 				continue;
 			}
