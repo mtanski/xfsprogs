3a01aadc32e30e0569511f691c5a12bc0a1e11f0
Alex Elder <aelder@sgi.com>
2010-01-28 10:37:38 -0500

    xfsprogs: digitally sign release tags
    Arrange for the commit marking a new release to include a
    "Signed-off-by" line, and have the corresponding tag include
    a digital signature.
    
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Alex Elder <aelder@sgi.com>

--- release.sh
+++ release.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Automate generation a new release
 #
 
-. VERSION
+. ./VERSION
 
 version=${PKG_MAJOR}.${PKG_MINOR}.${PKG_REVISION}
 date=`date +"%-d %B %Y"`
@@ -13,10 +13,10 @@ sed -e "s/${version}.*/${version} (${date})/" doc/CHANGES > doc/CHANGES.tmp && \
 	mv doc/CHANGES.tmp doc/CHANGES
 
 echo "Commiting CHANGES update to git"
-git-commit -a -m "${version} release"
+git commit -s -a -m "${version} release"
 
 echo "Tagging git repository"
-git-tag v${version}
+git tag -s -a -m "${version} release" v${version}
 
 echo "Creating source tarball"
 make dist
