3229e20820cf71aa18b67b2ad19d8d53d7b562b2
Ben Myers <bpm@sgi.com>
2012-12-13 14:33:42 -0600

    xfsprogs: update version to 3.1.10
    Update version to 3.1.10 and update release script to create a
    source tarball.
    
    Signed-off-by: Ben Myers <bpm@sgi.com>
    Reviewed-by: Alex Elder <elder@inktank.com>

--- release.sh
+++ release.sh
@@ -18,6 +18,10 @@ git commit -s -a -m "${version} release"
 echo "Tagging git repository"
 git tag -s -a -m "${version} release" v${version}
 
-echo "Done.  Please remember to push out tags using \"git push --tags\""
-echo "If you wish to create a source tarball, run \"make dist\""
+echo "Making source tarball"
+make dist
 
+echo "Sign the source tarball"
+gpg --detach-sign xfsprogs-${version}.tar.gz
+
+echo "Done.  Please remember to push out tags using \"git push --tags\""
