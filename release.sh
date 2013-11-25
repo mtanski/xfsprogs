05aa7620c6ead42389142c34bd15b867bc88f992
Rich Johnston <rjohnston@sgi.com>
2013-11-25 13:23:25 -0600

    3.2.0-alpha2 release

--- release.sh
+++ release.sh
@@ -16,15 +16,15 @@ sed -e "s/${version}.*/${version} (${date})/" doc/CHANGES > doc/CHANGES.tmp && \
 	mv doc/CHANGES.tmp doc/CHANGES
 
 echo "Commiting CHANGES update to git"
-git commit -s -a -m "${version} release"
+git commit -a -m "${version} release"
 
 echo "Tagging git repository"
-git tag -s -a -m "${version} release" v${version}
+git tag -a -m "${version} release" v${version}
 
 echo "Making source tarball"
 make dist
 
-echo "Sign the source tarball"
-gpg --detach-sign xfsprogs-${version}.tar.gz
+#echo "Sign the source tarball"
+#gpg --detach-sign xfsprogs-${version}.tar.gz
 
 echo "Done.  Please remember to push out tags using \"git push --tags\""
